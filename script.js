c52d483e1bac0c5a2a31ff072746da4cc5c7fbfe
spogsta <cheffjeff10@gmail.com>
2022-09-29 16:37:53 -0500

    Added an array dialogue func and ghetto btn func

--- script.js
+++ script.js
@@ -1,38 +1,40 @@
 
 let i = "";
 let y = 0;
+let dialogueCounter = 0;
 
 let speed = 50;
 
 function typeWriter(i) {
-    let z=i;
     
-    let x=z.length;
+    
+    let x=i.length;
 
     if (y===0){
         document.getElementById('textBox').innerHTML = ""
     }
     if ( y<x){
           
-        console.log(x) 
-        console.log(y)
-        console.log(z[y])
+      
         let d =document.getElementById('textBox').innerHTML
-        d = d + z[y];
-        y++
-        console.log(d)
-        
+        d = d + i[y];
+        y++;
         document.getElementById('textBox').innerHTML = d;  
-        setTimeout(()=>{ typeWriter(z)}, speed)
+        setTimeout(()=>{ typeWriter(i)}, speed)
     }
     else {
      y = 0;
+     document.getElementById('textBox').innerHTML=document.getElementById('textBox').innerHTML + "<button id='nextButton' onclick='pushBtn()'>" + buttons[dialogueCounter] + "</button>";
+     dialogueCounter ++;
+     
     }
 };
 
 
 function pushBtn(){
-     typeWriter("where am I?")
+     typeWriter(dialogue[dialogueCounter])
 }
 
 
+let dialogue = ["well you look like one!", "Man, for an orc you sure talk funny!"]
+let buttons = ["uhh....okay?", "listen...I'm really not an or-"]
